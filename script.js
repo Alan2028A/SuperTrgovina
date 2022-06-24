9c9e889dc30c3b0c1c2f5434e7baac9705405914
Alan2028A <alan.hudik28gmail.com>
2022-06-24 10:00:56 +0200

    dodao sam sliku i jos jedan potreban dokument, verzija alfa

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("test javaskripta");
+var varijabla = document.getElementById("nekiId");
+
+
+const datum = new Date();
+const vrijeme = datum.getTime();
+var trenutnoVrijeme = 0;
+
+var mojInterval = window.setInterval(updateVrijeme, 1000);
+
+
+function updateVrijeme()
+{
+	if(trenutnoVrijeme >= 4)
+    {
+        clearInterval(mojInterval);
+
+    }
+
+
+	const trenutniDatum = new Date();
+	trenutnoVrijeme = (trenutniDatum.getTime() - vrijeme) / 1000;
+	varijabla.innerHTML = Math.round(trenutnoVrijeme);
+}
